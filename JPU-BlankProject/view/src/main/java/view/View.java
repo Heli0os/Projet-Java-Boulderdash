5028919fd810209cd3f36186456f041cf8022df1
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-05-27 00:47:14 +0200

    Update View.java

--- JPU-BlankProject/view/src/main/java/view/View.java
+++ JPU-BlankProject/view/src/main/java/view/View.java
@@ -1,82 +1,31 @@
 package view;
 
-import java.awt.event.KeyEvent;
-
-import javax.swing.SwingUtilities;
-
-import contract.ControllerOrder;
+import java.util.Observer;
 import contract.IController;
-import contract.IModel;
 import contract.IView;
 
 /**
  * The Class View.
  *
- * @author Jean-Aymeric Diet
+ * @author Clément
  */
-public final class View implements IView, Runnable {
+public class View implements IView {
 
-	/** The frame. */
-	private final ViewFrame viewFrame;
+	private ViewFrame frame;
 
-	/**
-	 * Instantiates a new view.
-	 *
-	 * @param model
-	 *          the model
-	 */
-	public View(final IModel model) {
-		this.viewFrame = new ViewFrame(model);
-		SwingUtilities.invokeLater(this);
+	public View() {
+		this.frame = new ViewFrame();
 	}
 
-	/**
-	 * Key code to controller order.
-	 *
-	 * @param keyCode
-	 *          the key code
-	 * @return the controller order
-	 */
-	protected static ControllerOrder keyCodeToControllerOrder(final int keyCode) {
-		switch (keyCode) {
-			case KeyEvent.VK_G:
-				return ControllerOrder.English;
-			case KeyEvent.VK_F:
-				return ControllerOrder.Francais;
-			case KeyEvent.VK_D:
-				return ControllerOrder.Deutsch;
-			case KeyEvent.VK_I:
-				return ControllerOrder.Indonesia;
-			default:
-				return ControllerOrder.English;
-		}
+	public void repaint() {
+
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IView#printMessage(java.lang.String)
-	 */
-	public void printMessage(final String message) {
-		this.viewFrame.printMessage(message);
+	public Observer getObserver() {
+		return this.frame.getViewPanel();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.lang.Runnable#run()
-	 */
-	public void run() {
-		this.viewFrame.setVisible(true);
-	}
-
-	/**
-	 * Sets the controller.
-	 *
-	 * @param controller
-	 *          the new controller
-	 */
-	public void setController(final IController controller) {
-		this.viewFrame.setController(controller);
+	public void setController(IController controller) {
+		this.frame.setController(controller);
 	}
 }
