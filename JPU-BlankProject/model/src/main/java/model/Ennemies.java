b886d9e862cb8c1bef30ab19ed81969625e49da0
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-05-27 16:51:39 +0200

    Update Ennemies.java

--- JPU-BlankProject/model/src/main/java/model/Ennemies.java
+++ JPU-BlankProject/model/src/main/java/model/Ennemies.java
@@ -6,10 +6,7 @@ import contract.Direction;
  * @author Clément
  */
 public class Ennemies extends MotionElements {
-    private String spriteName = "Ennemy";
-
     public Ennemies (int x, int y, Direction direction) {
-        super (x, y, "");
-        this.direction = direction;
+        super (x, y, direction.LEFT, "Ennemy", "");
     }
 }
