92d23ddeeacedd6a4cce5ec4af6abda6143ba9d5
Rabatt09 <montemonttheophile@gmail.com>
2019-05-27 14:40:52 +0200

    temporary removed falling method and cleaned the detectFallingElements method

--- JPU-BlankProject/controller/src/main/java/controller/FallingController.java
+++ JPU-BlankProject/controller/src/main/java/controller/FallingController.java
@@ -24,39 +24,16 @@ public class FallingController {
     private void detectFallingElements() {
         for (int x = this.model.getWidth() - 1; x >= 0; x--) {
             for (int y = this.model.getHeight() -1; y >+ 0; y++) {
-                Elements element = this.model.getLocation()[x][y];
-            }
+                Elements element = this.model.getElement(x,y);
 
-            String spriteName = element.getSpriteName();
 
-            if(spriteName == "Boulder" || spriteName == "Diamond") {
-                this.Falling(x,y);
+
+
+                if(element.getSpriteName() == "Rock" || element.getSpriteName == "Diamond") {
+                    element.Falling(x,y);
+                }
             }
         }
     }
     
-    /**
-     * Manages the falling movement of the rocks and diamonds
-     * @param x
-     * Position x
-     * @param y
-     * Position y
-     */
-private void Falling(int x, int y) {
-        Elements elementDown = this.model.getLocation()[x][y+1];
-
-        String spriteNameDown = elementDown.getSpriteName();
-
-        if (spriteNameDown == "Digged") {
-            element.setLocation()[x][y+1];
-        }
-        else if (spriteNameDown == "Rock") {
-            if (this.model.getLocation()[x-1][y+1].getSpriteName == "Digged" && this.model.getLocation()[x-1][y].getSpriteName == "Digged") {
-                element.setLocation()[x-1][y];
-            }
-            else if (this.model.getLocation()[x+1][y+1].getSpriteName == "Digged" && this.model.getLocation()[x+1][y].getSpriteName == "Digged") {
-                element.setLocation()[x+1][y];
-            }
-        }
-    }
 }
