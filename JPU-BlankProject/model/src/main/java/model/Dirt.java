638062349b9545e14b32ebec3360cf55ea479767
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-05-27 16:50:36 +0200

    Update Dirt.java

--- JPU-BlankProject/model/src/main/java/model/Dirt.java
+++ JPU-BlankProject/model/src/main/java/model/Dirt.java
@@ -4,12 +4,7 @@ package model.Elements;
  * @author Clément
  */
 public class Dirt extends Elements {
-    private String spriteName;
-
     public Dirt (int x, int y) {
-        super (x, y, "");
-        this.spriteName = "Dirt";
+        super (x, y, "Dirt", "");
     }
-
-
 }
