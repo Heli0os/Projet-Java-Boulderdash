11da7820e7302efff782bbb886fd133d55581555
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-05-27 16:49:50 +0200

    Update Diamonds.java

--- JPU-BlankProject/model/src/main/java/model/Diamonds.java
+++ JPU-BlankProject/model/src/main/java/model/Diamonds.java
@@ -4,10 +4,8 @@ package model.Elements;
  * @author Clément
  */
 public class Diamonds extends Elements {
-    private String spriteName;
-
     public Diamonds (int x, int y) {
-        super (x, y, "");
-        this.spriteName = "Diamonds";
+        super (x, y, "Diamonds", "");
     }
 }
+
