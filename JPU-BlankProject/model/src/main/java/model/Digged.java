31b7c778769ff6932c4f9e2f69b31caacce625b8
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-05-27 16:50:14 +0200

    Update Digged.java

--- JPU-BlankProject/model/src/main/java/model/Digged.java
+++ JPU-BlankProject/model/src/main/java/model/Digged.java
@@ -4,12 +4,7 @@ package model.Elements;
  * @author Clément
  */
 public class Digged extends Elements {
-    private String spriteName;
-
     public Digged (int x, int y) {
-        super (x, y, "");
-        this.spriteName = "Digged";
+        super (x, y, "Digged", "");
     }
-
-
 }
