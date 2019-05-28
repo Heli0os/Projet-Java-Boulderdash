2197749f037e8bbd66e40991b440ea33010c3700
Rabatt09 <montemonttheophile@gmail.com>
2019-05-28 09:11:21 +0200

    Fixed for error and method order

--- JPU-BlankProject/model/src/main/java/model/FallingElements.java
+++ JPU-BlankProject/model/src/main/java/model/FallingElements.java
@@ -8,17 +8,7 @@ public class FallingElements extends Elements {
     Elements elementRight = getElement(x+1,y);
     Elements elementLUnder = getElement(x-1,y+1);
     Elements elementRUnder = getElement(x+1,y+1);
-    public void falling() {
 
-        /*
-        Falling function just move the current object down and replace his older position with "digged"
-         */
-        if (elementUnder.spriteName == "Digged") {
-            this.y++;
-            setElement(model.Elements.Digged, this.x, this.y);
-            this.fallingStatus = true;
-        }
-    }
     public void rolling(){
         /*
         Make falling element roll to fall aside
@@ -33,6 +23,19 @@ public class FallingElements extends Elements {
         }
     }
 
+    public void falling() {
+
+        /*
+        Falling function just move the current object down and replace his older position with "digged"
+         */
+        if (elementUnder.spriteName == "Digged") {
+            this.y++;
+            setElement(model.Elements.Digged, this.x, this.y);
+            this.fallingStatus = true;
+        }
+    }
+
+
     public void cruching(){
         if(this.fallingStatus == true && elementUnder.spriteName == "Player"){
            for(int i=x-;i<=x+1;i++){
@@ -43,8 +46,8 @@ public class FallingElements extends Elements {
            /*restart function here*/
         }
         if(this.fallingStatus == true && elementUnder.spriteName == "Enemy"){
-            for(int i=x-;i<=x+1;i++){
-                for(int j=y-;j<=y+1;j++){
+            for(int i=x-1;i<=x+1;i++){
+                for(int j=y-1;j<=y+1;j++){
                     setElement(model.Elements.Digged,i,j);
                 }
             }
