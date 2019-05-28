4fe2ba034c36c08b15e8ba8bcbcf1c5678b719b1
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-05-28 15:15:09 +0200

    Adding Element drawing on the map

--- JPU-BlankProject/view/src/main/java/view/ViewPanel.java
+++ JPU-BlankProject/view/src/main/java/view/ViewPanel.java
@@ -1,11 +1,11 @@
 package view;
 
 import java.awt.Graphics;
+import java.awt.image.ImageObserver;
 import java.util.Observable;
 import java.util.Observer;
-import contract.IModel;
-import contract.ILevel;
 import javax.swing.JPanel;
+import contract.*;
 
 /**
  * The Class ViewPanel.
@@ -18,18 +18,12 @@ class ViewPanel extends JPanel implements Observer {
 	private IModel model;
 	/** The level. */
 	private ILevel level;
+	private IController controller;
 	/** The view frame. */
-
 	private ViewFrame viewFrame;
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = -998294702363713521L;
 
-	/**
-	 * Instantiates a new view panel.
-	 *
-	 * @param viewFrame
-	 *          the view frame
-	 */
 	public ViewPanel() {
 		setVisible(true);
 	}
@@ -69,13 +63,24 @@ class ViewPanel extends JPanel implements Observer {
 	 */
 	@Override
 	protected void paintComponent(final Graphics graphics) {
-		if (this.model.getLevel().isFinished()) {
+		graphics.drawString("Player control : Z (UP), Q (LEFT), S (DOWN), D (RIGHT)", 450, 500);
+
+		for (int y = 0; y<level.getDimensions().getHeight(); y++) {
+			for (int x = 0; x<level.getDimensions().getWidth(); x++) {
+				IElements elements = level.getElement(x,y);
+				graphics.drawImage(elements.getImage(),x,y,this);
+			}
+		}
+		if (this.model.getLevel().isFinished() && this.model.getLevel().getPlayer().isAlive()) {
 			graphics.drawString("Congratulations on winning this level", 300, 400);
 			graphics.drawString("Number of diamonds collected : "+model.getLevel().getDiamondsCollected() +"/ "+model.getLevel().getDiamondsNumber(), 300, 500);
 			graphics.drawString("The game is now loading the next level", 300, 600);
 			model.getLevel().setDiamondsCollected(0);
 		}
-		else if (!this.model.getLevel().getPlayer.isAlive()) {
+		else if (this.controller.isGamePaused()) {
+			graphics.drawString("The game is Paused, press echap to resume"+model.getLevel().getDiamondsCollected() +"/ "+model.getLevel().getDiamondsNumber(), 300, 500);
+		}
+		else if (!this.model.getLevel().getPlayer().isAlive()) {
 			graphics.drawString("You died, the game is lost", 400, 450);
 			graphics.drawString("Number of diamonds collected : "+model.getLevel().getDiamondsCollected() +"/ "+model.getLevel().getDiamondsNumber(), 300, 550);
 			model.getLevel().setDiamondsCollected(0);
