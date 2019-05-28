6af09485f71d4667910661a96611ffd51c02265c
Rabatt09 <montemonttheophile@gmail.com>
2019-05-28 14:28:32 +0200

    addes getter for the map itself

--- JPU-BlankProject/model/src/main/java/model/GameMap.java
+++ JPU-BlankProject/model/src/main/java/model/GameMap.java
@@ -4,11 +4,13 @@ import model.Elements.Elements;
 
 public class GameMap {
 
-    Elements[][] map;
+    public Elements[][] map;
 
     public Elements getElement(int x,int y){
         return map[x][y];
     }
+
+    public Elements[][] getElements(){return map;}
 
     public void setElement(Elements element,int x,int y){
         map[x][y]= element;
