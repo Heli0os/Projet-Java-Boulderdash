9bcbdeb333b9238942e22a740be3783206d925f6
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-05-29 14:01:38 +0200

    Update IPlayer.java

--- JPU-BlankProject/contract/src/main/java/contract/IPlayer.java
+++ JPU-BlankProject/contract/src/main/java/contract/IPlayer.java
@@ -1,7 +1,18 @@
 package contract;
 
+/**
+ * @author Clément 
+ */
 public interface IPlayer extends IMotionElements {
+    /**
+     * says if the player is alive or not
+     * @return
+     */
     boolean isAlive();
 
+    /**
+     * set the player as alive or not
+     * @param isAlive
+     */
     void setIsAlive (boolean isAlive);
 }
