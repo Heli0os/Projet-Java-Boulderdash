4017d54259c11a8c82146e857eb3cb1583ab97b1
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-05-29 14:24:37 +0200

    Update View.java

--- JPU-BlankProject/view/src/main/java/view/View.java
+++ JPU-BlankProject/view/src/main/java/view/View.java
@@ -11,24 +11,44 @@ import contract.IView;
  */
 public class View implements IView {
 
+	/**
+	 * The viewFrame
+	 */
 	private ViewFrame frame;
 
+	/**
+	 * The constructor of the view
+	 */
 	public View() {
 		this.frame = new ViewFrame();
 	}
 
+	/**
+	 * repaint the view
+	 */
 	public void repaint() {
 
 	}
 
+	/**
+	 * close the view
+	 */
 	public void close() {
 		this.frame.dispose();
 	}
 
+	/**
+	 * Get the observer
+	 * @return The observer
+	 */
 	public Observer getObserver() {
 		return this.frame.getViewPanel();
 	}
 
+	/**
+	 * Set the controller
+	 * @param controller The controller
+	 */
 	public void setController(IController controller) {
 		this.frame.setController(controller);
 	}
