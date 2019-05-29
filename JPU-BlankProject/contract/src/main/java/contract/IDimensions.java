b7996ffbcdf4f8fcafc2b12454ba81df484a5b50
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-05-29 13:42:26 +0200

    Update IDimensions.java

--- JPU-BlankProject/contract/src/main/java/contract/IDimensions.java
+++ JPU-BlankProject/contract/src/main/java/contract/IDimensions.java
@@ -1,12 +1,32 @@
 package contract;
 
+/**
+ * @author Clément 
+ */
 public interface IDimensions {
+
+    /**
+     * set the Width
+     * @param width
+     */
     void setWidth(int width);
 
+    /**
+     * get the Width
+     * @return
+     */
     int getWidth();
 
+    /**
+     * set the Height
+     * @param height
+     */
     void setHeight(int height);
 
+    /**
+     * get the Height 
+     * @return
+     */
     int getHeight();
 
 }
