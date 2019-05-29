b91c44525e26b45d9d0f3a9a97e9899e7400601a
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-05-29 13:40:08 +0200

    Update IController.java

--- JPU-BlankProject/contract/src/main/java/contract/IController.java
+++ JPU-BlankProject/contract/src/main/java/contract/IController.java
@@ -14,9 +14,34 @@ public interface IController {
 	 *          the controller order
 	 */
 	void orderPerform(ControllerOrder controllerOrder);
+
+	/**
+	 * The game is paused or not
+	 * @return
+	 */
 	boolean isGamePaused();
+
+	/**
+	 * set the game paused or not
+	 * @param isGamePaused
+	 */
 	void setGamePaused(boolean isGamePaused);
+
+	/**
+	 * Start the game
+	 * @param levelIndex
+	 */
 	void start(int levelIndex);
+
+	/**
+	 * Function play that will launch the gameLoop
+	 * @param levelIndex
+	 */
 	void play(int levelIndex);
+
+	/**
+	 * Function that will launch movements while the game isn't finished or paused
+	 * @param levelIndex
+	 */
 	void gameLoop(int levelIndex);
 }
