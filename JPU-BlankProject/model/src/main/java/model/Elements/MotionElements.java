cf144dca9dc28d322dca0fa5ce06723fb418744f
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-06-01 13:20:20 +0200

    Adding javadoc

--- JPU-BlankProject/model/src/main/java/model/Elements/MotionElements.java
+++ JPU-BlankProject/model/src/main/java/model/Elements/MotionElements.java
@@ -4,21 +4,42 @@ import contract.Direction;
 import contract.IMotionElements;
 
 /**
+ * The class MotionElements
+ * 
  * @author Clément
  */
 public abstract class MotionElements extends Elements implements IMotionElements {
 
+    /**
+     * The direction
+     */
     protected Direction direction;
 
+    /**
+     * The constructor
+     * @param x The x position
+     * @param y The y position
+     * @param direction The direction
+     * @param name The name
+     * @param imagePath The image path
+     */
     public MotionElements (int x, int y, Direction direction, String name, String imagePath) {
         super(x, y, name, imagePath);
         this.direction = direction;
     }
 
+    /**
+     * get the direction
+     * @return the direction
+     */
     public Direction getDirection() {
         return direction;
     }
 
+    /**
+     * set the direction
+     * @param direction The direction
+     */
     public void setDirection (Direction direction) {
         this.direction = direction;
     }
