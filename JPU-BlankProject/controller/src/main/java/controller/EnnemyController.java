55dcd1753facac532c0510a03a27538d9e7500dc
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-06-01 12:41:54 +0200

    Adding javadoc

--- JPU-BlankProject/controller/src/main/java/controller/EnnemyController.java
+++ JPU-BlankProject/controller/src/main/java/controller/EnnemyController.java
@@ -12,12 +12,12 @@ import java.util.Random;
  * @author Clément / Théophile
  */
 public class EnnemyController {
-    
+
     /**
      * the EnnemyController
      */
     private static EnnemyController ec;
-    
+
     /**
      * The model
      */
@@ -67,13 +67,12 @@ public class EnnemyController {
     }
 
     /**
-     * Perform the movement
+     * Scan the level and perform the movement for the ennemies detected
      */
     protected void performMovement() {
         for (int x = model.getLevel().getDimensions().getHeight() - 1; x >= 0; x--) {
             for (int y = model.getLevel().getDimensions().getWidth() - 1; y >= 0; y--) {
                 IElements element = this.model.getLevel().getElement(x, y);
-
                 if (element instanceof Ennemies) {
                     moveControl.movementIsPossible(Direction.getRandomDirection(), element.getX(), element.getY());
                 }
